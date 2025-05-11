34a24e6c52d51d586ef27c01e23a55ce98ebbbef
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-05-12 00:28:22 +0300

    up

---
+++ src/animated_line/AnimateLine.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../animated_line/animated_line.css'
+export default function AnimateLine() {
+  return (
+    <div className="parent">
+      <div className="square"></div>
+   <div className="line"></div>
+      <div className="square"></div>
+    </div>
+  )
+}
